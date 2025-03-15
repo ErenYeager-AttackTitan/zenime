0fb993628df9a6e8ce0ef6f7ad12e2b40dceca2d
ErenYeager-AttackTitan <eren.yeager.founding.titan.9@gmail.com>
2025-03-15 21:40:03 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -72,7 +72,7 @@ function Navbar() {
               to="/"
               className="text-4xl font-bold max-[575px]:text-3xl cursor-pointer"
             >
-              {logoTitle.slice(0, 6)}
+              {logoTitle.slice(0, 5)}
               <span className="text-[#FFBADE]">{logoTitle.slice(5, 6)}</span>
               {logoTitle.slice(6)}
             </Link>
