0c5e238b303c25ecb5e018961b15249bf88aef3a
ErenYeager-AttackTitan <eren.yeager.founding.titan.9@gmail.com>
2025-03-23 13:55:18 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -127,7 +127,7 @@ function Navbar() {
             </div>
           </div>
           <Link
-            to="https://t.me/zenime_discussion"
+            to="https://t.me/animezia_support"
             className="flex flex-col gap-y-1 items-center cursor-pointer"
           >
             <FaTelegramPlane
